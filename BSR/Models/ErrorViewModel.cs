ca9726e4ccbd60a412ada24e9fe9b805e987ec39
Bence Ilcsik <bence.ilcsik@capgemini.com>
2020-09-10 00:30:33 +0200

    BSR-1 - add initial project files

---
+++ BSR/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BSR.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
