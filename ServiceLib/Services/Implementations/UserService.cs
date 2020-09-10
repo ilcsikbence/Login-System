aff6d9bc7412474a92820dc308bbd70b5a38ac65
Bence Ilcsik <bence.ilcsik@capgemini.com>
2020-09-10 10:02:55 +0200

    BSR-6 - add UserService to ServiceLib

---
+++ ServiceLib/Services/Implementations/UserService.cs
@@ -0,0 +1,11 @@
+﻿using ServiceLib.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceLib.Services.Implementations
+{
+    public class UserService : IUserService, IService
+    {
+    }
+}
---
+++ ServiceLib/Services/Interfaces/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceLib.Services.Interfaces
+{
+    public interface IUserService
+    {
+    }
+}
